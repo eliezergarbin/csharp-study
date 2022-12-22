588b80382248cc1a541459ace44677cc16ab4422
Eliezer Nascimento <eliezergarbin1@gmail.com>
2022-12-22 16:34:30 -0300

    Primeiro exercicio

---
+++ Course/Course/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Course
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
